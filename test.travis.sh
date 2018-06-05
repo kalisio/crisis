922e4649b97fc81877a73efd705f9c2a4c15fa5d
Luc Claustres <luc.claustres@orange.fr>
2018-06-05 14:17:15 +0200

    Try to add travis tests output to S3 bucket

--- test.travis.sh
+++ test.travis.sh
@@ -7,8 +7,9 @@ else
 	# Required to avoid local build of the image
 	docker pull kalisio/aktnmap:${FLAVOR}
 	docker-compose -f docker-compose.yml up -d mongodb-aktnmap
+	mkdir server-coverage
 	docker-compose -f docker-compose.yml -f docker-compose.server-tests.yml up aktnmap
+	mkdir client-screenshots
 	docker-compose -f docker-compose.yml -f docker-compose.client-tests.yml up -d aktnmap
 	docker-compose -f docker-compose.yml -f docker-compose.client-tests.yml up testcafe
-	codeclimate-test-reporter < server-coverage/lcov.info
 fi
