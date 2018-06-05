4e60778c1949ca5b32f5c0dd7e629af7d779dd9c
Luc Claustres <luc.claustres@orange.fr>
2018-06-05 15:49:24 +0200

    Try to fix client tests output to S3 bucket [skip build][skip android][skip ios]

--- test.travis.sh
+++ test.travis.sh
@@ -7,9 +7,13 @@ else
 	# Required to avoid local build of the image
 	docker pull kalisio/aktnmap:${FLAVOR}
 	docker-compose -f docker-compose.yml up -d mongodb-aktnmap
+	# Output directory for server coverage
 	mkdir server-coverage
+	chmod -R 666 server-coverage
 	docker-compose -f docker-compose.yml -f docker-compose.server-tests.yml up aktnmap
+	# Output directory for client screenshots
 	mkdir client-screenshots
+	chmod -R 666 client-screenshots
 	docker-compose -f docker-compose.yml -f docker-compose.client-tests.yml up -d aktnmap
 	docker-compose -f docker-compose.yml -f docker-compose.client-tests.yml up testcafe
 fi
