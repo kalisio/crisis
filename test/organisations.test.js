5ce0d03571324644322642ac96a5462ae8944318
Christophe Nouguier <christophe.nouguier@free.fr>
2018-06-07 08:19:50 +0200

    Fixed create organisation test

--- test/organisations.test.js
+++ test/organisations.test.js
@@ -40,7 +40,7 @@ test('Create organisation', async test => {
   await organisations.createOrganisation(test, data.organisation)
   // We should have the created organisation in the organisations panel
   // FIXME: innerText contains an additionnal \n which makes the test fail
-  await test.expect(organisations.appBarTitle.innerText).eql(data.organisation.name + '\n', 'AppBar title should be the organisation name')
+  await test.expect(organisations.appBarTitle.innerText).eql(data.organisation.name, 'AppBar title should be the organisation name')
   const panel = await organisations.panel.getVue()
   await test.expect(panel.state.items.length).eql(2, 'New organisation should be added to the panel')
   await pages.checkNoClientError(test)
