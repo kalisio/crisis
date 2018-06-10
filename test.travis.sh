139b1b980cb772474b24df345aeae5c826dbd722
Luc Claustres <luc.claustres@orange.fr>
2018-06-10 22:00:21 +0200

    Try to make tests skippable

--- test.travis.sh
+++ test.travis.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Output directory for server coverage
+mkdir server-coverage
+chmod -R 777 server-coverage
+# Output directory for client screenshots
+mkdir client-screenshots
+chmod -R 777 client-screenshots
+		
 if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip test]"* ]]
 then
 	echo "Skipping test stage"
@@ -7,13 +14,7 @@ else
 	# Required to avoid local build of the image
 	docker pull kalisio/aktnmap:${FLAVOR}
 	docker-compose -f docker-compose.yml up -d mongodb-aktnmap
-	# Output directory for server coverage
-	mkdir server-coverage
-	chmod -R 777 server-coverage
 	docker-compose -f docker-compose.yml -f docker-compose.server-tests.yml up aktnmap
 	docker-compose -f docker-compose.yml -f docker-compose.client-tests.yml up -d aktnmap
-	# Output directory for client screenshots
-	mkdir client-screenshots
-	chmod -R 777 client-screenshots
 	docker-compose -f docker-compose.yml -f docker-compose.client-tests.yml up testcafe
 fi
