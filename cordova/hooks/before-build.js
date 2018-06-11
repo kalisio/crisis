535a18292d98c6168758ddeffc68c0649b65354e
Luc Claustres <luc.claustres@orange.fr>
2018-06-11 13:33:18 +0200

    Try to fix android build issue [skip build][skip test][skip deploy]

---
+++ cordova/hooks/before-build.js
@@ -0,0 +1,23 @@
+module.exports = function(context) {
+	// Copy any build extras
+	console.log('Copying build extras')
+	var path = context.requireCordovaModule('path')
+	var fs = context.requireCordovaModule('fs')
+	fs.copyFileSync(path.join(__dirname, '..', 'build-extras.gradle'),
+	path.join(__dirname, '..', 'platforms', 'android', 'build-extras.gradle'))
+
+	// Required to manage this issue https://github.com/zo0r/react-native-push-notification/issues/748
+	var Q = context.requireCordovaModule('q')
+    var defer = new Q.defer()
+	fs.readFile(path.join(__dirname, '..', 'platforms', 'android', 'build.gradle'), (error, data) => {
+		if (error) {
+			console.log(error)
+			defer.resolve()
+			return
+		}
+		console.log('Updating build file')
+		var buildFile = data.toString().replace(/jcenter\(\)/g, 'maven { url "https://maven.google.com" }\n\tjcenter()')
+		fs.writeFileSync(path.join(__dirname, '..', 'platforms', 'android', 'build.gradle'), buildFile)
+		defer.resolve()
+	})
+}
--- cordova/hooks/build-extras.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(context) {
-	console.log('Copying build extras')
-	var path = context.requireCordovaModule('path')
-	var fs = context.requireCordovaModule('fs')
-	fs.copyFileSync(path.join(__dirname, '..', 'build-extras.gradle'),
-	path.join(__dirname, '..', 'platforms', 'android', 'build-extras.gradle'))
-}
