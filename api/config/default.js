588c561d5840b45d14849f0bf12b65533b7f87ee
Christophe Nouguier <christophe.nouguier@free.fr>
2018-08-28 16:05:51 +0200

    Updated config

--- api/config/default.js
+++ api/config/default.js
@@ -212,7 +212,8 @@ module.exports = {
     plans: {
       // First plan is the default one
       bronze: {
-        color: 'light-green-4'
+        color: 'light-green-4',
+        default: true
       },
       silver: {
         color: 'light-green-6',
