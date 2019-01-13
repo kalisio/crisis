d0433e7a698973d857cc1bf93ed9d07ea424b8c0
Christophe Nouguier <christophe.nouguier@free.fr>
2019-01-13 20:38:31 +0100

    Updated dependencies version Fixed lint issue [skip ci]

--- api/src/main.js
+++ api/src/main.js
@@ -8,30 +8,30 @@ const N = parseInt(process.env.NODE_APP_NB_INSTANCES)
 let server
 
 if (cluster.isMaster && (N > 1)) {
-	logger.info(`Launching master with pid ${process.pid}`);
+  logger.info(`Launching master with pid ${process.pid}`)
 
   for (let i = 0; i < N; i++) {
     cluster.fork()
   }
 } else {
-	server = new Server()
+  server = new Server()
 
-	const config = server.app.get('logs')
-	const logPath = _.get(config, 'DailyRotateFile.dirname')
-	if (logPath) {
-	  // This will ensure the log directory does exist
-	  fs.ensureDirSync(logPath)
-	}
+  const config = server.app.get('logs')
+  const logPath = _.get(config, 'DailyRotateFile.dirname')
+  if (logPath) {
+    // This will ensure the log directory does exist
+    fs.ensureDirSync(logPath)
+  }
 
-	if (require.main === module) {
-	  process.on('unhandledRejection', (reason, p) =>
-	    logger.error('Unhandled Rejection at: Promise ', p, reason)
-	  )
+  if (require.main === module) {
+    process.on('unhandledRejection', (reason, p) =>
+      logger.error('Unhandled Rejection at: Promise ', p, reason)
+    )
 
-	  server.run().then(() => {
-	    logger.info(`Server with pid ${process.pid} started listening`)
-	  })
-	}
+    server.run().then(() => {
+      logger.info(`Server with pid ${process.pid} started listening`)
+    })
+  }
 }
 
 export default server
