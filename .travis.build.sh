b509ace3c4a4730264793d1317812acb9d51637b
Christophe Nouguier <christophe.nouguier@free.fr>
2019-03-17 22:46:37 +0100

    build: wip #56

--- .travis.build.sh
+++ .travis.build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 source .travis.env.sh
 
+# It first need to create the required network 
+docker network create --attachable $DOCKER_NETWORK
+
+#
+# Build the app
+#
+travis_fold start "build"
+
 # NOTE: The process build the image and run the container in order to allow us to copy the 
 # built artifact from the container to the host. Indeed the artifact is then copied to S3 
 # (see the deploy hook) and can be used by the following stages (i.e. Android and iOS).
-
-# It first need to create the required network 
-docker network create --attachable $DOCKER_NETWORK
 
 if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip build]"* ]]
 then
@@ -22,12 +27,19 @@ else
 	if [ $? -eq 1 ]; then
 	  exit 1
   fi
-	
+
 	# Tag the built image and push it to the hub
 	docker tag kalisio/$APP kalisio/$APP:$VERSION_TAG
 	docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
 	docker push kalisio/$APP:$VERSION_TAG
 fi
+
+travis_fold end "build"
+
+#
+#  Backup the artifact to S3
+#
+travis_fold start "backup"
 
 # Copy the artifact from the container to the host
 # See https://docs.docker.com/compose/reference/envvars/#compose_project_name
@@ -39,4 +51,6 @@ aws s3 sync dist s3://$BUILDS_BUCKET/$BUILD_NUMBER/www > /dev/null
 if [ $? -eq 1 ]; then
 	exit 1
 fi
+
+travis_fold end "backup"
 
