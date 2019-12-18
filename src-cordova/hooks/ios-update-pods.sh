c894563f56e260fabe4d276aaba1683c4bac311e
Christophe Nouguier <christophe.nouguier@free.fr>
2019-12-18 19:23:46 +0100

    wip: Cannot install phonegap-plugin-push anymore #93 [skip-test]

--- src-cordova/hooks/ios-update-pods.sh
+++ src-cordova/hooks/ios-update-pods.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#pod repo update
+# Please refer to: 
+# https://github.com/phonegap/phonegap-plugin-push/issues/2845
+# https://github.com/CocoaPods/CocoaPods/issues/6223#issuecomment-262277995
+# Should be temporary and fixed in the future with:
+# https://github.com/apache/cordova-ios/pull/719
 pod setup
 
 mkdir -p ~/.cocoapods/repos
