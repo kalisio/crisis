e8e1655c84b8506bb501241bb794bb4801106877
Luc Claustres <luc.claustres@orange.fr>
2020-01-09 19:15:57 +0100

    WIP https://github.com/kalisio/kMap/issues/55 (2D layer edition component)

--- src/components/LayerStyleForm.vue
+++ src/components/LayerStyleForm.vue
@@ -83,6 +83,50 @@
               label label-always :label-value="$t('LayerStyleForm.LINE_OPACITY') + defaultLine['stroke-opacity']"/>
           </q-item-section>
         </q-item>
+        <q-item v-for="lineStyle in lineStyles" :key="lineStyle.key" class="col-12">
+          <q-item-section class="col-4">
+            <component
+              :is="lineStyle.componentKey"
+              :ref="lineStyle.key"
+              :properties="lineStyle.properties"
+              :display="{ icon: true, label: true, labelWidth: 3 }"
+              @field-changed="lineStyle.onValueChanged"
+            />
+          </q-item-section>
+          <q-item-section class="col-1">
+            <q-btn round small :color="lineStyle['stroke-color']" @click="onColorClicked(lineStyle, 'stroke-color')">
+            </q-btn>
+          </q-item-section>
+          <q-item-section class="col-3">
+            <q-slider v-model="lineStyle['stroke-width']"
+              :min="1" :max="20" :step="1"
+              label label-always :label-value="$t('LayerStyleForm.LINE_WIDTH') + lineStyle['stroke-width'] + 'px'"/>
+          </q-item-section>
+          <q-item-section class="col-3">
+            <q-slider v-model="lineStyle['stroke-opacity']"
+              :min="0" :max="1" :step="0.1"
+              label label-always :label-value="$t('LayerStyleForm.LINE_OPACITY') + lineStyle['stroke-opacity']"/>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-btn flat color="primary" icon="delete" @click="onRemoveLineStyle(lineStyle)">
+              <q-tooltip anchor="top middle" self="bottom middle" :offset="[10, 10]">
+                {{$t('LayerStyleForm.REMOVE_LINE_STYLE')}}
+              </q-tooltip>
+            </q-btn>
+          </q-item-section>
+        </q-item>
+        <q-item class="col-12">
+          <q-item-section avatar class="col-6">
+            {{$t('LayerStyleForm.ADD_LINE_STYLE')}}
+          </q-item-section>
+          <q-item-section class="col-6">
+            <q-select v-model="property" :options="properties">
+              <template v-slot:after>
+                <q-btn v-if="property" round dense flat icon="add" @click="onAddLineStyle(property)"/>
+              </template>
+            </q-select>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-expansion-item>
     <q-expansion-item ref="polygons" icon="fas fa-draw-polygon" :label="$t('LayerStyleForm.POLYGONS')" group="group">
@@ -99,6 +143,45 @@
             <q-slider v-model="defaultPolygon['fill-opacity']"
               :min="0" :max="1" :step="0.1"
               label label-always :label-value="$t('LayerStyleForm.POLYGON_FILL_OPACITY') + defaultPolygon['fill-opacity']"/>
+          </q-item-section>
+        </q-item>
+        <q-item v-for="polygonStyle in polygonStyles" :key="polygonStyle.key" class="col-12">
+          <q-item-section class="col-4">
+            <component
+              :is="polygonStyle.componentKey"
+              :ref="polygonStyle.key"
+              :properties="polygonStyle.properties"
+              :display="{ icon: true, label: true, labelWidth: 3 }"
+              @field-changed="polygonStyle.onValueChanged"
+            />
+          </q-item-section>
+          <q-item-section class="col-1">
+            <q-btn round small :color="polygonStyle['fill-color']" @click="onColorClicked(polygonStyle, 'fill-color')">
+            </q-btn>
+          </q-item-section>
+          <q-item-section class="col-3">
+            <q-slider v-model="polygonStyle['fill-opacity']"
+              :min="0" :max="1" :step="0.1"
+              label label-always :label-value="$t('LayerStyleForm.POLYGON_FILL_OPACITY') + polygonStyle['fill-opacity']"/>
+          </q-item-section>
+          <q-item-section avatar>
+            <q-btn flat color="primary" icon="delete" @click="onRemovePolygonStyle(polygonStyle)">
+              <q-tooltip anchor="top middle" self="bottom middle" :offset="[10, 10]">
+                {{$t('LayerStyleForm.REMOVE_POLYGON_STYLE')}}
+              </q-tooltip>
+            </q-btn>
+          </q-item-section>
+        </q-item>
+        <q-item class="col-12">
+          <q-item-section avatar class="col-6">
+            {{$t('LayerStyleForm.ADD_LINE_STYLE')}}
+          </q-item-section>
+          <q-item-section class="col-6">
+            <q-select v-model="property" :options="properties">
+              <template v-slot:after>
+                <q-btn v-if="property" round dense flat icon="add" @click="onAddPolygonStyle(property)"/>
+              </template>
+            </q-select>
           </q-item-section>
         </q-item>
       </q-list>
@@ -182,6 +265,12 @@ export default {
     },
     hasIconStyles () {
       return this.iconStyles.length > 0
+    },
+    hasLineStyles () {
+      return this.lineStyles.length > 0
+    },
+    hasPolygonStyles () {
+      return this.polygonStyles.length > 0
     }
   },
   data () {
@@ -211,12 +300,20 @@ export default {
       await this.$nextTick()
       this.hasError = false
     },
+    async loadStyleComponents (styles) {
+      // Since we use dynamic component loading we need to make sure Vue.js has loaded them
+      // Set the refs to be resolved
+      this.setRefs(styles.map(style => style.key))
+      await this.loadRefs()
+      styles.forEach(style => this.$refs[style.key][0].fill(style.value))
+    },
     async fillClusteringStyle(values) {
       this.clustering = (_.get(values, 'leaflet.cluster', _.get(this.options, 'cluster')) ? true : false)
       this.disableClusteringAtZoom = _.get(values, 'leaflet.cluster.disableClusteringAtZoom',
         _.get(this.options, 'cluster.disableClusteringAtZoom', 18))
     },
     async fillIconStyles(values) {
+      this.iconStyles = []
       const templates = _.get(values, 'leaflet.template', [])
       // When no template we have a single default icon
       if (!templates.includes('marker-color') && !templates.includes('icon-classes')) {
@@ -239,7 +336,6 @@ export default {
         const propertyValueRegex = /"([^"]+)"/g
         const colorRegex = /%>([^<%]+)<%/g
         const nameRegex = /%>([^<%]+)<%/g
-        this.iconStyles = []
         let propertyValue
         while ((propertyValue = propertyValueRegex.exec(templateColors[0])) !== null) {
           const propertyName = propertyNameRegex.exec(templateColors[0])
@@ -250,16 +346,11 @@ export default {
             value: propertyValue[1].replace('"', '').trim()
           }))
         }
-        // Since we use dynamic component loading we need to make sure Vue.js has loaded them
-        // Set the refs to be resolved
-        this.setRefs(this.iconStyles.map(style => style.key))
-        await this.loadRefs()
-        this.iconStyles.forEach(style => {
-          this.$refs[style.key].fill(style.value)
-        })
+        await this.loadStyleComponents(this.iconStyles)
       }
     },
     async fillLineStyles(values) {
+      this.lineStyles = []
       const templates = _.get(values, 'leaflet.template', [])
       // When no template we have a single default style
       if (!templates.includes('stroke-color') && !templates.includes('stroke-width') && !templates.includes('stroke-opacity')) {
@@ -271,10 +362,40 @@ export default {
         this.defaultLine['stroke-opacity'] = _.get(values, 'leaflet.stroke-opacity',
           _.get(this.options, 'featureStyle.opacity', 1))
       } else {
-        // TODO
+        // Otherwise we have line styles for a set of values templated using if statements
+        // Split after else statement to get default line style
+        const templateColors = _.get(values, 'leaflet.stroke-color').split('} else {')
+        const templateWidths = _.get(values, 'leaflet.stroke-width').split('} else {')
+        const templateOpacities = _.get(values, 'leaflet.stroke-opacity').split('} else {')
+        // Conversion from palette to RGB color is required for markers
+        this.defaultLine['stroke-color'] = kCoreUtils.getPaletteFromColor(templateColors[1].match(/%>([^<%]+)<%/)[1])
+        this.defaultLine['stroke-width'] = templateWidths[1].match(/%>([^<%]+)<%/)[1]
+        this.defaultLine['stroke-opacity'] = templateOpacities[1].match(/%>([^<%]+)<%/)[1]
+        // Match properties equality to get property names
+        const propertyNameRegex = /properties.([^===]+)===/g
+        // Match quotes to get property values and %> <% to get icon colors/names
+        const propertyValueRegex = /"([^"]+)"/g
+        const colorRegex = /%>([^<%]+)<%/g
+        const widthRegex = /%>([^<%]+)<%/g
+        const opacityRegex = /%>([^<%]+)<%/g
+        let propertyValue
+        while ((propertyValue = propertyValueRegex.exec(templateColors[0])) !== null) {
+          const propertyName = propertyNameRegex.exec(templateColors[0])
+          const color = colorRegex.exec(templateColors[0])
+          const width = widthRegex.exec(templateWidths[0])
+          const opacity = opacityRegex.exec(templateOpacities[0])
+          this.lineStyles.push(this.createStyle(propertyName[1].trim(), {
+            'stroke-color': kCoreUtils.getPaletteFromColor(color[1].trim()),
+            'stroke-width': Number(width[1].trim()),
+            'stroke-opacity': Number(opacity[1].trim()),
+            value: propertyValue[1].replace('"', '').trim()
+          }))
+        }
+        await this.loadStyleComponents(this.lineStyles)
       }
     },
     async fillPolygonStyles(values) {
+      this.polygonStyles = []
       const templates = _.get(values, 'leaflet.template', [])
       // When no template we have a single default style
       if (!templates.includes('fill-color') && !templates.includes('fill-opacity')) {
@@ -284,7 +405,31 @@ export default {
         this.defaultPolygon['fill-opacity'] = _.get(values, 'leaflet.fill-opacity',
           _.get(this.options, 'featureStyle.fillOpacity', 1))
       } else {
-        // TODO
+        // Otherwise we have polygon styles for a set of values templated using if statements
+        // Split after else statement to get default polygon style
+        const templateColors = _.get(values, 'leaflet.fill-color').split('} else {')
+        const templateOpacities = _.get(values, 'leaflet.fill-opacity').split('} else {')
+        // Conversion from palette to RGB color is required for markers
+        this.defaultPolygon['fill-color'] = kCoreUtils.getPaletteFromColor(templateColors[1].match(/%>([^<%]+)<%/)[1])
+        this.defaultPolygon['fill-opacity'] = templateOpacities[1].match(/%>([^<%]+)<%/)[1]
+        // Match properties equality to get property names
+        const propertyNameRegex = /properties.([^===]+)===/g
+        // Match quotes to get property values and %> <% to get icon colors/names
+        const propertyValueRegex = /"([^"]+)"/g
+        const colorRegex = /%>([^<%]+)<%/g
+        const opacityRegex = /%>([^<%]+)<%/g
+        let propertyValue
+        while ((propertyValue = propertyValueRegex.exec(templateColors[0])) !== null) {
+          const propertyName = propertyNameRegex.exec(templateColors[0])
+          const color = colorRegex.exec(templateColors[0])
+          const opacity = opacityRegex.exec(templateOpacities[0])
+          this.polygonStyles.push(this.createStyle(propertyName[1].trim(), {
+            'fill-color': kCoreUtils.getPaletteFromColor(color[1].trim()),
+            'fill-opacity': Number(opacity[1].trim()),
+            value: propertyValue[1].replace('"', '').trim()
+          }))
+        }
+        await this.loadStyleComponents(this.polygonStyles)
       }
     },
     async fillPopupStyles(values) {
@@ -317,6 +462,7 @@ export default {
       await this.fillPopupStyles(values)
       // Tooltip
       await this.fillTooltipStyles(values)
+      
     },
     validate () {
       logger.debug('Validating layer style form')
@@ -356,17 +502,51 @@ export default {
     },
     lineStylesValues() {
       let values = {}
-      // Conversion from palette to RGB color is required for markers
-      values['leaflet.stroke-color'] = kCoreUtils.getColorFromPalette(this.defaultLine['stroke-color'])
-      values['leaflet.stroke-width'] = this.defaultLine['stroke-width']
-      values['leaflet.stroke-opacity'] = this.defaultLine['stroke-opacity']
+      let colorTemplate = '', widthTemplate = '', opacityTemplate = ''
+      this.lineStyles.forEach(style => {
+        // Conversion from palette to RGB color is required for styles
+        const color = kCoreUtils.getColorFromPalette(style['stroke-color'])
+        const width = style['stroke-width']
+        const opacity = style['stroke-opacity']
+        const property = style.property
+        const value = style.value
+        colorTemplate += `if (properties.${property} === "${value}") { %>${color}<% } else `
+        widthTemplate += `if (properties.${property} === "${value}") { %>${width}<% } else `
+        opacityTemplate += `if (properties.${property} === "${value}") { %>${opacity}<% } else `
+      })
+      // Conversion from palette to RGB color is required for styles
+      const color = kCoreUtils.getColorFromPalette(this.defaultLine['stroke-color'])
+      const width = this.defaultLine['stroke-width']
+      const opacity = this.defaultLine['stroke-opacity']
+      colorTemplate += (this.hasLineStyles ? `{ %>${color}<% }` : `${color}`)
+      widthTemplate +=  (this.hasLineStyles ? `{ %>${width}<% }` : `${width}`)
+      opacityTemplate +=  (this.hasLineStyles ? `{ %>${opacity}<% }` : `${opacity}`)
+      values['leaflet.stroke-color'] = (this.hasLineStyles ? `<% ${colorTemplate} %>` : `${colorTemplate}`)
+      values['leaflet.stroke-width'] = (this.hasLineStyles ? `<% ${widthTemplate} %>` : `${widthTemplate}`)
+      values['leaflet.stroke-opacity'] = (this.hasLineStyles ? `<% ${opacityTemplate} %>` : `${opacityTemplate}`)
+      values['leaflet.template'] = (this.hasLineStyles ? ['stroke-color', 'stroke-width', 'stroke-opacity'] : [])
       return values
     },
     polygonStylesValues() {
       let values = {}
-      // Conversion from palette to RGB color is required for markers
-      values['leaflet.fill-color'] = kCoreUtils.getColorFromPalette(this.defaultPolygon['fill-color'])
-      values['leaflet.fill-opacity'] = this.defaultPolygon['fill-opacity']
+      let colorTemplate = '', opacityTemplate = ''
+      this.polygonStyles.forEach(style => {
+        // Conversion from palette to RGB color is required for styles
+        const color = kCoreUtils.getColorFromPalette(style['fill-color'])
+        const opacity = style['fill-opacity']
+        const property = style.property
+        const value = style.value
+        colorTemplate += `if (properties.${property} === "${value}") { %>${color}<% } else `
+        opacityTemplate += `if (properties.${property} === "${value}") { %>${opacity}<% } else `
+      })
+      // Conversion from palette to RGB color is required for styles
+      const color = kCoreUtils.getColorFromPalette(this.defaultPolygon['fill-color'])
+      const opacity = this.defaultPolygon['fill-opacity']
+      colorTemplate += (this.hasPolygonStyles ? `{ %>${color}<% }` : `${color}`)
+      opacityTemplate +=  (this.hasPolygonStyles ? `{ %>${opacity}<% }` : `${opacity}`)
+      values['leaflet.fill-color'] = (this.hasPolygonStyles ? `<% ${colorTemplate} %>` : `${colorTemplate}`)
+      values['leaflet.fill-opacity'] = (this.hasPolygonStyles ? `<% ${opacityTemplate} %>` : `${opacityTemplate}`)
+      values['leaflet.template'] = (this.hasPolygonStyles ? ['fill-color', 'fill-opacity'] : [])
       return values
     },
     popupStylesValues() {
@@ -381,14 +561,19 @@ export default {
     },
     values () {
       let values = {}
+      // Be default lodash merges objects only not arrays
+      // As the template style property is one we need this
+      const customizer = (objValue, srcValue) => {
+        if (_.isArray(objValue)) return objValue.concat(srcValue)
+      }
       // Clustering
       _.merge(values, this.clusteringValues())
       // Point style
-      _.merge(values, this.iconStylesValues())
+      _.mergeWith(values, this.iconStylesValues(), customizer)
       // Line style
-      _.merge(values, this.lineStylesValues())
+      _.mergeWith(values, this.lineStylesValues(), customizer)
       // Polygon style
-      _.merge(values, this.polygonStylesValues())
+      _.mergeWith(values, this.polygonStylesValues(), customizer)
       // Popup style
       _.merge(values, this.popupStylesValues())
       // Tooltip style
@@ -405,13 +590,18 @@ export default {
       }
       let style = {
         key: uid().toString(), componentKey, property, properties,
-        icon: { name: 'fas fa-circle', color: 'blue' }, // Default icon
         onValueChanged: (field, value) => style.value = value
       }
       return Object.assign(style, options)
     },
     onAddIconStyle (property) {
-      this.iconStyles.push(this.createStyle(property.value))
+      this.iconStyles.push(this.createStyle(property.value, {
+        // Default icon
+        icon: {
+          name: _.get(this.options, 'pointStyle.icon.options.iconClasses', 'fas fa-circle'),
+          color: _.get(this.options, 'pointStyle.icon.options.markerColor', 'blue')
+        },
+      }))
     },
     onRemoveIconStyle (style) {
       // Required to update the array to make it reactive
@@ -431,6 +621,29 @@ export default {
     },
     onColorChanged (color) {
       _.set(this.editedStyle, this.editedColor, color)
+    },
+    onAddLineStyle (property) {
+      this.lineStyles.push(this.createStyle(property.value, {
+        // Default line
+        'stroke-color': _.get(this.options, 'featureStyle.color', 'red'),
+        'stroke-width': _.get(this.options, 'featureStyle.weight', 1),
+        'stroke-opacity': _.get(this.options, 'featureStyle.opacity', 1)
+      }))
+    },
+    onRemoveLineStyle (style) {
+      // Required to update the array to make it reactive
+      this.lineStyles = this.lineStyles.filter(item => item.key !== style.key)
+    },
+    onAddPolygonStyle (property) {
+      this.polygonStyles.push(this.createStyle(property.value, {
+        // Default line
+        'fill-color': _.get(this.options, 'featureStyle.fillColor', 'green'),
+        'fill-opacity': _.get(this.options, 'featureStyle.fillOpacity', 1)
+      }))
+    },
+    onRemovePolygonStyle (style) {
+      // Required to update the array to make it reactive
+      this.polygonStyles = this.polygonStyles.filter(item => item.key !== style.key)
     }
   },
   async created () {
