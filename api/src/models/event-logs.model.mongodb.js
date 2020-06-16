48782733a60a472fb8e33b92dd3acfa95bb28fdc
Luc Claustres <luc.claustres@orange.fr>
2020-06-16 15:33:01 +0200

    Updated indices for event archiving

--- api/src/models/event-logs.model.mongodb.js
+++ api/src/models/event-logs.model.mongodb.js
@@ -4,6 +4,6 @@ module.exports = function (app, options) {
   // Expire at a given date
   options.Model.createIndex({ expireAt: 1 }, { expireAfterSeconds: 0 })
   // Use compound index so that we can easily filter by event then by participant
-  options.Model.createIndex({ event: 1, participant: 1 })
+  options.Model.createIndex({ event: 1, participant: 1, lastInEvent: true })
   options.Model.createIndex({ geometry: '2dsphere' })
 }
