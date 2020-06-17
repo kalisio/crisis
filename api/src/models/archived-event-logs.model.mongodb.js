d5a04f7ae3e1cde8228e86ff769a743c12d8b6de
Luc Claustres <luc.claustres@orange.fr>
2020-06-17 09:50:51 +0200

    Fixed event logs index [skip ci]

--- api/src/models/archived-event-logs.model.mongodb.js
+++ api/src/models/archived-event-logs.model.mongodb.js
@@ -2,6 +2,6 @@ module.exports = function (app, options) {
   const db = options.db || app.db
   options.Model = db.collection('archived-event-logs')
   // Use compound index so that we can easily filter by event then by participant
-  options.Model.createIndex({ event: 1, participant: 1, lastInEvent: true })
+  options.Model.createIndex({ event: 1, participant: 1, lastInEvent: 1 })
   options.Model.createIndex({ geometry: '2dsphere' })
 }
