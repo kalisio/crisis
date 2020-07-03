f7cab43c538e1587b5eaf3d37a7654cd6f12e088
Luc Claustres <luc.claustres@orange.fr>
2020-07-03 15:05:44 +0200

    Fixed search index in events activity

--- api/src/models/events.model.mongodb.js
+++ api/src/models/events.model.mongodb.js
@@ -7,4 +7,8 @@ module.exports = function (app, options) {
   options.Model.createIndex({ updatedAt: -1 })
   options.Model.createIndex({ deletedAt: -1 })
   options.Model.createIndex({ geometry: '2dsphere' })
+  // Collation provided in query ensure sorting to be case insensitive w.r.t. user's language
+  // We built indices with collation to cover the most used languages, it requires different naming...
+  options.Model.createIndex({ name: 1 }, { name: 'name-en', collation: { locale: 'en', strength: 1 } })
+  options.Model.createIndex({ name: 1 }, { name: 'name-fr', collation: { locale: 'fr', strength: 1 } })
 }
