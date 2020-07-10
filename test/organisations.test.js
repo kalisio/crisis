f272975060f392010a5c59bbf026f694ec060f54
Christophe Nouguier <christophe.nouguier@free.fr>
2020-07-10 23:58:16 +0200

    test: fixed organisations tests

--- test/organisations.test.js
+++ test/organisations.test.js
@@ -27,6 +27,7 @@ test('Create user and check default organisation', async test => {
   await screens.goToRegisterScreen(test)
   await screens.register(test, data.user)
   await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
   await layout.clickLeading(test)
   await organisationsPanel.checkCount(test, 1)
   await pages.checkNoClientError(test)
@@ -35,6 +36,7 @@ test('Create user and check default organisation', async test => {
 test('Forbid additional free org creation', async test => {
   await screens.login(test, data.user)
   await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
   await layout.clickLeading(test)
   await organisationsPanel.create(test, data.organisation.name, data.organisation.description)
   const error = await organisationsPanel.isErrorVisible()
@@ -45,6 +47,7 @@ test('Forbid additional free org creation', async test => {
 test('Delete default organisation', async test => {
   await screens.login(test, data.user)
   await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
   await layout.clickOverflowMenu(test, pages.OrganisationSettings.OVERFLOW_MENU_ENTRY)
   await layout.clickTabBar(test, pages.OrganisationSettings.DANGER_ZONE_TAB)
   await organisationSettings.delete(test, data.user.name)
@@ -53,34 +56,46 @@ test('Delete default organisation', async test => {
   await pages.checkNoClientError(test)
 })
 
-test.skip('Create organisation', async test => {
-  await app.loginAndCloseSignupAlert(test, data.user)
-  await organisations.createOrganisation(test, data.organisation)
-  await test.expect(organisations.appBarTitle.innerText).eql(data.organisation.name, 'AppBar title should be the organisation name')
-  await organisations.checkOrganisationCount(test, 1)
+test('Create organisation', async test => {
+  await screens.login(test, data.user)
+  await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
+  await layout.clickLeading(test)
+  await organisationsPanel.create(test, data.organisation.name, data.organisation.description)
+  //await test.expect(organisations.appBarTitle.innerText).eql(data.organisation.name, 'AppBar title should be the organisation name')
+  await organisationsPanel.checkCount(test, 1)
   await pages.checkNoClientError(test)
 })
 
-test.skip('Forbid account deletion', async test => {
-  await app.loginAndCloseSignupAlert(test, data.user)
-  // Cannot remove the account because the user is still owning an organisation
-  await account.removeAccount(test, data.user.name)
-  const error = await organisations.isErrorVisible()
+test('Forbid account deletion', async test => {
+  await screens.login(test, data.user)
+  await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
+  await layout.clickLeading(test)
+  await sideNav.clickIdentity(test)
+  await layout.clickTabBar(test, pages.Account.DANGER_ZONE_TAB)
+  await account.delete(test, data.user.name)
+  const error = await organisationSettings.isErrorVisible()
   await test.expect(error).ok('Forbidden error should be displayed')
   await pages.checkClientError(test)
 })
 
-test.skip('Delete created organisation', async test => {
-  await app.loginAndCloseSignupAlert(test, data.user)
-  await organisations.deleteOrganisation(test, data.organisation.name)
-  // We should have the deleted organisation removed from the organisations panel
-  await organisations.checkOrganisationCount(test, 0)
+test('Delete created organisation', async test => {
+  await screens.login(test, data.user)
+  await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
+  await layout.clickOverflowMenu(test, pages.OrganisationSettings.OVERFLOW_MENU_ENTRY)
+  await layout.clickTabBar(test, pages.OrganisationSettings.DANGER_ZONE_TAB)
+  await organisationSettings.delete(test, data.organisation.name)
+  await layout.clickLeading(test)
+  await organisationsPanel.checkCount(test, 0)
   await pages.checkNoClientError(test)
 })
 
 test('Delete user', async test => {
   await screens.login(test, data.user)
   await layout.closeSignupAlert(test)
+  await layout.closeWelcomeDialog(test)
   await layout.clickLeading(test)
   await sideNav.clickIdentity(test)
   await layout.clickTabBar(test, pages.Account.DANGER_ZONE_TAB)
