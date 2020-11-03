f7681831c41f1eb8a7ea27b445664a95b1c751ee
Luc Claustres <luc.claustres@orange.fr>
2020-11-03 16:32:19 +0100

    Fixed https://github.com/kalisio/aktnmap/issues/178

--- api/src/models/events.model.mongodb.js
+++ api/src/models/events.model.mongodb.js
@@ -7,6 +7,9 @@ module.exports = function (app, options) {
   options.Model.createIndex({ updatedAt: -1 })
   options.Model.createIndex({ deletedAt: -1 })
   options.Model.createIndex({ geometry: '2dsphere' })
+  // We can only have a single event related to a given alert so we apply a uniicity constraint
+  // However most events are not linked to an alert so we need to avoid MongoDB applyint it on null values
+  options.Model.createIndex({ 'alert._id': 1 }, { unique: true, partialFilterExpression: { 'alert._id': { $exists: true } } })
   // Collation provided in query ensure sorting to be case insensitive w.r.t. user's language
   // We built indices with collation to cover the most used languages, it requires different naming...
   options.Model.createIndex({ name: 1 }, { name: 'name-en', collation: { locale: 'en', strength: 1 } })
