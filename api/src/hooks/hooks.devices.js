013ca9d67e055aa1f4b74cd4e1f6d8cd93e3ca88
Luc Claustres <luc.claustres@orange.fr>
2020-12-07 16:05:08 +0100

    Fixed https://github.com/kalisio/aktnmap/issues/192

--- api/src/hooks/hooks.devices.js
+++ api/src/hooks/hooks.devices.js
@@ -1,11 +1,13 @@
 export async function subscribeToAppTopic (hook) {
   const topicsObject = hook.app.get('pusher')
+  // The app topics are actually stored in the pusher config
   if (topicsObject) {
     const pusherService = hook.app.getService('pusher')
     await pusherService.create({
       action: 'subscriptions'
     }, {
-      pushObject: topicsObject,
+      // Pusher usually expects objects coming from the DB so add an _id to avoid any problem
+      pushObject: Object.assign({ _id: 'aktnmap' }, topicsObject),
       users: [hook.params.user]
     })
   }
@@ -14,11 +16,13 @@ export async function subscribeToAppTopic (hook) {
 
 export async function unsubscribeFromAppTopic (hook) {
   const topicsObject = hook.app.get('pusher')
+  // The app topics are actually stored in the pusher config
   if (topicsObject) {
     const pusherService = hook.app.getService('pusher')
     await pusherService.remove(null, {
       query: { action: 'subscriptions' },
-      pushObject: topicsObject,
+      // Pusher usually expects objects coming from the DB so add an _id to avoid any problem
+      pushObject: Object.assign({ _id: 'aktnmap' }, topicsObject),
       users: [hook.params.user]
     })
   }
