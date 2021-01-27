915eeda4ce7ab7d64b3c1a7b295be2f1e91df3f3
Luc Claustres <luc.claustres@orange.fr>
2021-01-27 16:01:58 +0100

    chore: Updated deployment to run kargo pull/configure

--- .travis.app.sh
+++ .travis.app.sh
@@ -74,7 +74,7 @@ cp workspace/$FLAVOR/ssh.config ~/.ssh/config
 # Deploy the stack except in prod
 if [ $FLAVOR != "prod" ]
 then
-	ssh REMOTE_SERVER "cd kargo; ./kargo remove $APP; ./kargo deploy $APP; ./kargo exec test-$APP"
+	ssh REMOTE_SERVER 'export BUILD_BUCKET='"'$BUILD_BUCKET'"';'"cd kargo; ./kargo remove $APP; ./kargo pull; ./kargo configure; ./kargo deploy $APP; ./kargo exec test-$APP"
 	check_code $? "Deploying the app"
 fi
 
