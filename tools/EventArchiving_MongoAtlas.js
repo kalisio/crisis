a21695a7424fb36fb2028871901ed43fd0491a20
Luc Claustres <luc.claustres@orange.fr>
2021-04-27 21:53:13 +0200

    chore: updated real service name for new production cluster in archiving functions [skip ci]

--- tools/EventArchiving_MongoAtlas.js
+++ tools/EventArchiving_MongoAtlas.js
@@ -3,7 +3,7 @@ exports = async function(dbEvent) {
   const id = dbEvent.documentKey._id
   const dbName = dbEvent.ns.db
   const collectionName = dbEvent.ns.coll
-  const collection = context.services.get("aktnmap-prod").db(dbName).collection("archived-" + collectionName)
+  const collection = context.services.get("prod").db(dbName).collection("archived-" + collectionName)
   let event = dbEvent.fullDocument
   // On deletion do not delete archived event but tag it with deletion date
   if (dbEvent.operationType.toLowerCase() == "delete")
@@ -14,7 +14,7 @@ exports = async function(dbEvent) {
   {
     // Try to categorize event to the best matching template
     if (!event.template) {
-      const templateCollection = context.services.get("aktnmap-prod").db(dbName).collection("event-templates")
+      const templateCollection = context.services.get("prod").db(dbName).collection("event-templates")
       const results = await templateCollection.find(
          { $text: { $search: event.name } },
          { score: { $meta: "textScore" } }
