72c9a104a36d57c24852d3a1c5fe5bbb63db155e
Luc Claustres <luc.claustres@orange.fr>
2021-07-23 18:35:00 +0200

    wip: Provide capabilities to manage plans - fixed sync issue in plan mixin (#261)

--- src/components/PlanMenu.vue
+++ src/components/PlanMenu.vue
@@ -1,43 +1,41 @@
 <template>
-  <div>
-    <q-btn-dropdown v-show="planId"
-      class="q-pl-sm ellipsis-2-lines"
-      :label="label"
-      :icon="icon"
-      :size="size"
-      flat
-      dense
-      no-caps
-      square
-      menu-anchor="bottom middle"
-      menu-self="top middle">
-      <template v-slot:default>
-        <q-card class="bg-white" style="width: 300px;">
-          <div class="row full-width justify-center items-center q-pa-md q-gutter-x-sm text-subtitle1 bg-grey-4">
-            <k-avatar 
-              :class="$q.screen.lt.sm ? 'q-pa-none' : 'q-pa-xs'" 
-              :object="plan" 
-              :contextId="organisation._id"
-              size="sm" />
-            <div>
-              {{ plan.name }}
-            </div>
+  <q-btn-dropdown v-show="planId"
+    class="q-pl-sm ellipsis-2-lines"
+    :label="label"
+    :icon="icon"
+    :size="size"
+    flat
+    dense
+    no-caps
+    square
+    menu-anchor="bottom middle"
+    menu-self="top middle">
+    <template v-slot:default>
+      <q-card class="bg-white" style="width: 300px;">
+        <div class="row full-width justify-center items-center q-pa-md q-gutter-x-sm text-subtitle1 bg-grey-4">
+          <k-avatar 
+            :class="$q.screen.lt.sm ? 'q-pa-none' : 'q-pa-xs'" 
+            :object="plan" 
+            :contextId="organisation._id"
+            size="sm" />
+          <div>
+            {{ plan.name }}
           </div>
-          <div class="q-pa-xs">
-            <k-text-area class="light-paragraph q-pa-sm" :text="plan.description" />
-            <!-- Objectives section -->
-            <k-card-section>
-              <template v-for="(objective, index) in plan.objectives">
-                <div :key="objective" class="row full-width items-center justify-between">
-                  <q-toggle v-model="objectiveFilters[index]" :label="objective.value" />
-                </div>
-              </template>
-            </k-card-section>
-          </div>
-        </q-card>
-      </template>
-    </q-btn-dropdown>
-  </div>
+        </div>
+        <div class="q-pa-xs">
+          <k-text-area class="light-paragraph q-pa-sm" :text="plan.description" />
+          <!-- Objectives section -->
+          <k-card-section>
+            <template v-for="(objective, index) in plan.objectives">
+              <div :key="objective" class="row full-width items-center justify-between">
+                <q-toggle v-model="objectiveFilters[index]" :label="objective.value" />
+              </div>
+            </template>
+          </k-card-section>
+        </div>
+      </q-card>
+    </template>
+  </q-btn-dropdown>
 </template>
 
 <script>
@@ -56,7 +54,7 @@ export default {
   inject: ['kActivity'],
   computed: {
     label () {
-      return this.$q.screen.lt.md ? '' : this.plan.name
+      return (this.$q.screen.lt.md || !this.plan ? '' : this.plan.name)
     },
     icon () {
       return _.get(this.plan, 'icon.name', 'las la-stream')
@@ -80,11 +78,12 @@ export default {
   watch:{
     plan: {
       handler () {
-        this.objectiveFilters = _.fill(Array(this.plan.objectives.length), false)
+        if (this.plan) this.objectiveFilters = _.fill(Array(this.plan.objectives.length), false)
       }
     },
     objectiveFilters: {
       handler () {
+        if (!this.plan) return
         let filters = []
         for (let i = 0; i < this.objectiveFilters.length; i++) {
           if (this.objectiveFilters[i]) filters.push(_.get(this.plan, `objectives[${i}].value`))
