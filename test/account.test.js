8a283ee6e04de2bed30866bbc16b4b40beb28cc8
cnouguier <christophe.nouguier@free.fr>
2021-09-16 12:27:47 +0200

    test: updated account.test.js [skip ci]

--- test/account.test.js
+++ test/account.test.js
@@ -4,40 +4,46 @@ import { deleteOrganisation } from './organisations'
 
 const suite = 'layout'
 
-const runnerOptions = {
-  browser: {
-    slowMo: 1
-  }
-}
-
 describe(suite, () => {
   let runner
   let page
-  let user
+  let user = {
+    name: 'Account Tester',
+    email: 'account.test@kalisio.xyz',
+    password: 'Pass;word1'
+  }
 
   before(async () => {
-    runner = new core.Runner(suite, runnerOptions)
+    runner = new core.Runner(suite, {
+      browser: {
+        slowMo: 1
+      }
+    })
     page = await runner.start()
+  })
+
+  it ('create-account', async () => {
     await core.goToRegisterScreen(page)
-    user = {
-      name: 'Account Tester',
-      email: 'account.test@kalisio.xyz',
-      password: 'Pass;word1'
-    }
     await core.register(page, user.name, user.email, user.password)
-    //await core.login(page, user.email, user.password)
     await core.closeSignupAlert(page)
     await core.closeWelcomeDialog(page)
   })
 
+  it('check-account-deletion-forbidden', async () => {
+    await core.deleteAccount(page, user.name)
+    // TODO catch error messages
+  })
+
   it('delete-organisation', async () => {
-    //expect(await core.countCards(page) === 1).to.true
     await deleteOrganisation(page, user.name)
     expect(await core.countCards(page) === 0).to.true
   })
 
-  after(async () => {
+  it('delete-account', async () => {
     await core.deleteAccount(page, user.name)
+  })
+
+  after(async () => {
     await runner.stop()
   })
 })
