e0c63577bf64460f340690a611e5bd1b42b1166c
cnouguier <christophe.nouguier@free.fr>
2021-09-17 11:36:29 +0200

    test: updated account test suite [skip ci]

--- test/account.test.js
+++ test/account.test.js
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
 import { core } from '@kalisio/kdk/test.client'
-import { deleteOrganisation } from './organisations'
+import { countOrganisations, organisationExists, createOrganisation, deleteOrganisation } from './organisations'
 
-const suite = 'layout'
+const suite = 'account'
 
 describe(suite, () => {
   let runner
@@ -22,14 +22,26 @@ describe(suite, () => {
     page = await runner.start()
   })
 
-  it ('create-account', async () => {
+  it('create-account', async () => {
     await core.goToRegisterScreen(page)
     await core.register(page, user.name, user.email, user.password)
     await core.closeSignupAlert(page)
     await core.closeWelcomeDialog(page)
   })
 
-  it('check-account-deletion-forbidden', async () => {
+  it('check-user-has-one-organisation', async () => {
+    expect(await organisationExists(page, user.name)).to.true
+    expect(await countOrganisations(page) === 1).to.true
+  })
+
+  it('check-organisation-creation-is-forbidden', async () => {
+    await createOrganisation(page, 'Dummy', 'Dummy organisation')
+    expect(runner.hasError()).to.true
+    runner.clearErrors()
+    await core.clickAction(page, 'cancel-button')
+  })
+
+  it('check-account-deletion-is-forbidden', async () => {
     await core.deleteAccount(page, user.name)
     expect(runner.hasError()).to.true
     runner.clearErrors()
@@ -37,15 +49,18 @@ describe(suite, () => {
 
   it('delete-organisation', async () => {
     await deleteOrganisation(page, user.name)
-    expect(await core.countCards(page) === 0).to.true
+    expect(await countOrganisations(page) === 0).to.true
   })
 
   it('delete-account', async () => {
     await core.deleteAccount(page, user.name)
   })
 
-  after(async () => {
+  it('check-console', () => {
     expect(runner.hasError()).to.false
+  })
+
+  after(async () => {
     await runner.stop()
   })
 })
--- test/organisations.js
+++ test/organisations.js
@@ -1,10 +1,33 @@
 import { core } from '@kalisio/kdk/test.client'
 
-export async function deleteOrganisation (page, name) {
-  await core.clickLeftPaneAction(page, 'my-organisations', 1000) // wait for the collection to be rendered
+export async function goToOrganisationsActivity (page, wait = 2000) {
+  const url = page.url()
+  if (!url.includes('organisations')) await core.clickLeftPaneAction(page, 'my-organisations', wait)
+}
+
+export async function countOrganisations (page) {
+  await goToOrganisationsActivity(page)
+  await page.waitForTimeout(2000)
+  return core.countCards(page)
+}
+
+export async function organisationExists (page, name) {
+  await goToOrganisationsActivity(page)
+  return core.cardExists(page, name)
+}
+
+export async function createOrganisation (page, name, description, wait = 1000) {
+  await goToOrganisationsActivity(page)
+  await core.clickAction(page, 'create-organisation')
+  await core.type(page, '#name-field', name)
+  await core.type(page, '#description-field', description)
+  await core.clickAction(page, 'apply-button', wait)
+}
+
+export async function deleteOrganisation (page, name, wait = 1000) {
+  await goToOrganisationsActivity(page)
   await core.clickCardAction(page, name, 'expand-action', 1000) // wait for the card to be opened
   await core.clickCardAction(page, name, 'remove-item-header')
   await core.type(page, '.q-dialog input', name)
-  await core.click(page, '.q-dialog button:nth-child(2)')
-  await page.waitForTimeout(2000)
+  await core.click(page, '.q-dialog button:nth-child(2)', wait)
 }
