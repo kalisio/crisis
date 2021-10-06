c9a9f36e9e6db68a5fbf1ad81deb4eeb1c145974
cnouguier <christophe.nouguier@free.fr>
2021-10-06 10:08:03 +0200

    ci: homogenize travis.app.sh

--- .travis.app.sh
+++ .travis.app.sh
@@ -15,10 +15,6 @@ travis_fold start "provision"
 
 source .travis.env.sh
 
-
-docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
-check_code $? "Connecting to Docker"
-
 travis_fold end "provision"
 
 #
@@ -36,7 +32,11 @@ BUILD_CODE=$?
 tail -n 24 build.log
 check_code $BUILD_CODE "Builing the client" 
 
-# Create an archive to speed docker build process
+# Log in to docker before building the app because of rate limiting
+docker login -u="$DOCKER_USER" -p="$DOCKER_PASSWORD"
+check_code $? "Connecting to Docker"
+
+# Create an archive to speed docker build process and build the image
 cd ../..
 tar -zcf kalisio.tgz kalisio
 docker build --build-arg APP=$APP --build-arg FLAVOR=$FLAVOR --build-arg BUILD_NUMBER=$BUILD_NUMBER -f dockerfile -t kalisio/$APP:$TAG . 
