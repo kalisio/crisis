3db0571fe37169233bca77cdb15da3acd739cf18
Tristan Greffe <tristan.greffe@hotmail.com>
2023-07-20 18:05:18 +0200

    wip: Progressive Web App - improve push notification delivery #421

--- api/src/utils.js
+++ api/src/utils.js
@@ -2,30 +2,30 @@ import _ from 'lodash'
 
 // Send a notification to participants that can be users, groups or tags
 export async function sendPushNotifications (app, participants, notification) {
-  const userService = app.getService('users')
   const pushService = app.getService('push')
-  if (!pushService) return
+  if (!pushService) return hook
+
   // Define data for notification
   _.defaults(notification, {
-    // FIXME: add vibration
     icon: 'https://s3.eu-central-1.amazonaws.com/kalisioscope/aktnmap/aktnmap-icon-512x512.png'
   })
-  // Define participants
-  const users = await userService.find({ paginate: false })
-  const participantsId = []
-  _.forEach(participants, participant => {
-    if (participant.service === 'members') participantsId.push(participant._id)
-    if (participant.service === 'groups' || participant.service === 'tags') {
-      _.forEach(users, user => {
-        if (_.find(user[participant.service], participantService => _.toString(participantService._id) === _.toString(participant._id))) participantsId.push(user._id)
-      })
-    }
-  })
-  // Send notification
-  pushService.create({
+  const data = {
     notification, 
     subscriptionService: 'api/users',
     subscriptionProperty: 'subscriptions',
-    subscriptionFilter: _.isEmpty(participants) ? {} : { _id: { $in: participantsId }}
+  }
+
+  // Define participants
+  const [usersId, groupsId, tagsId] = Array(3).fill([])
+  _.forEach(participants, participant => {
+    if (participant.service === 'members') usersId.push(participant._id)
+    if (participant.service === 'groups') groupsId.push(participant._id)
+    if (participant.service === 'tags') tagsId.push(participant._id)
   })
+
+
+  // Send notification
+  if (_.size(usersId) > 0) pushService.create({ ...data, subscriptionFilter: { _id: { $in: usersId }}})
+  if (_.size(groupsId) > 0) pushService.create({ ...data, subscriptionFilter: { groups: { $elemMatch: { _id: { $in: groupsId }}}}})
+  if (_.size(tagsId) > 0) pushService.create({ ...data, subscriptionFilter: { tags: { $elemMatch: { _id: { $in: tagsId }}}}})
 }
