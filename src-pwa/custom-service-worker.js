f177d237112b797d15713e5f256fa8da167c098d
cnouguier <ch.nouguier@gmail.com>
2023-09-14 13:27:00 +0200

    restore: default caching strategy

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,7 +1,6 @@
-// import { precacheAndRoute } from 'workbox-precaching'
+import { precacheAndRoute } from 'workbox-precaching'
 import { registerRoute } from 'workbox-routing'
-import { NetworkOnly } from 'workbox-strategies'
-// import {ExpirationPlugin} from 'workbox-expiration'
+import { NetworkFirst } from 'workbox-strategies'
 
 // Disable workbox logs 
 self.__WB_DISABLE_DEV_LOGS = true
@@ -15,23 +14,11 @@ self.addEventListener('message', event => {
 
 // Caching for offline mode
 // Preload and cache all resources defined in the manifest
-//precacheAndRoute(self.__WB_MANIFEST)
-console.log(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST)
 // Register the `NetworkFirst` caching strategy for all HTTP requests
 registerRoute(
   ({url}) => url.href.startsWith('http'),
-  new NetworkOnly() /*{
-    plugins: [
-      new ExpirationPlugin({
-        // Keep at most 50 entries.
-        maxEntries: 1,
-        // Don't keep any entries for more than 30 days.
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-        // Automatically cleanup if quota is exceeded.
-        purgeOnQuotaError: true
-      })
-    ]
-  })*/
+  new NetworkFirst() 
 )
 
 // Web push notification
